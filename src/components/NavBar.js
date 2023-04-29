e6d960c982b2b49736788808c166fe8594cbceb5
Brayan Fuentes <39744430+BrayanFuentes@users.noreply.github.com>
2023-04-29 00:36:19 -0700

    initiated the navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function NavBar() {
+    return (
+        <>
+            <nav className = "navbar">
+                <div className="nav-container">
+
+                </div>
+
+            </nav>
+        </>
+    );
+}
+
+export default NavBar;
